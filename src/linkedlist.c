98110da47366833e402472b72d8f60b6186d1b8c
gustavopeq <gustavo_pequeno2@hotmail.com>
2020-03-27 17:57:28 -0700

    Create linkedlist added

--- src/linkedlist.c
+++ src/linkedlist.c
@@ -1,17 +1,61 @@
-/*
- ============================================================================
- Name        : linkedlist.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct node
+{
+	int data;
+	struct node *next;
+
+}node;
+
+node *createLinkedList(int size);
+
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+
+	node *headLinkedList = NULL;
+	headLinkedList = createLinkedList(5);
+
 	return EXIT_SUCCESS;
+}
+
+node *createLinkedList(int size)
+{
+	printf("Creating Linked list of size %d\n", size);
+	node *head = NULL;
+	node *newNode = NULL;
+
+	for (int i  = 0; i < size; i++)
+	{
+		newNode = (node*)malloc(sizeof(node));
+
+		if(newNode == NULL)
+		{
+			printf("Memory couldn't be allocated\n");
+			return NULL;
+		}else
+		{
+			printf("Data for node %d: ", i + 1);
+			fflush(stdout);
+			scanf("%d", &(newNode->data));
+			printf("\n");
+			newNode -> next = NULL;
+			if(head == NULL)
+			{
+				head = newNode;
+			}else
+			{
+				node *temp = head;
+
+				while(temp->next != NULL)
+				{
+					temp = temp->next;
+				}
+
+				temp -> next = newNode;
+			}
+
+		}
+	}
+
+	return head;
 }
